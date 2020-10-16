a8f9b2a1ca45507c48220ca2b109e60d0934783a
Fatih Karahan <hseyin97@gmail.com>
2020-10-16 17:30:53 +0300

    added post and user models and resolver for get posts, connected to mongodb atlas

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
+const mongoose = require("mongoose");
 
-const typeDefs = gql`
-  type Query {
-    sayHi: String!
-  }
-`;
+const { MONGODB } = require("./config");
 
-const resolvers = {
-  Query: {
-    sayHi: () => "Hello World!",
-  },
-};
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-server
-  .listen({
-    port: 5000,
+mongoose
+  .connect(MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+    return server.listen({
+      port: 5000,
+    });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
