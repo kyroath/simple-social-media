490d01ed7704684a8bb842232bf476e3d3223a35
Fatih Karahan <hseyin97@gmail.com>
2020-10-16 22:37:01 +0300

    added create/delete comment

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,6 +6,21 @@ module.exports = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
+  }
+
+  type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+  }
+
+  type Like {
+    id: ID!
+    createdAt: String!
+    username: String!
   }
 
   type User {
@@ -34,5 +49,10 @@ module.exports = gql`
 
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+
+    createComment(postId: String!, body: String!): Post!
+    deleteComment(postId: String!, commentId: String!): Post!
+
+    likePost(postId: String!): Post!
   }
 `;
