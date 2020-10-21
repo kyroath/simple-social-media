9713dbfafce4949406f7ac84a31b3c7b409def96
Fatih Karahan <hseyin97@gmail.com>
2020-10-21 16:21:40 +0300

    fetched and rendered the posts on the home page

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import ApolloProvider from "./ApolloProvider";
 
-ReactDOM.render(
-  <React.StrictMode>{ApolloProvider}</React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(ApolloProvider, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
