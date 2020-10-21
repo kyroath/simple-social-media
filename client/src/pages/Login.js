0537fcdfab630803a3d3c394eef041a283bff369
Fatih Karahan <hseyin97@gmail.com>
2020-10-21 17:33:34 +0300

    added react context to hold user data

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,13 @@
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/client/react/hooks";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Form } from "semantic-ui-react";
 
+import { AuthContext } from "../context/auth";
 import { useForm } from "../util/hooks";
 
 const Login = (props) => {
+  const context = useContext(AuthContext);
   const [errors, setErrors] = useState({});
 
   const { onChange, onSubmit, values } = useForm(loginUserCallback, {
@@ -14,7 +16,8 @@ const Login = (props) => {
   });
 
   const [loginUser, { loading }] = useMutation(LOGIN_USER, {
-    update(_, result) {
+    update(_, { data: { login: userData } }) {
+      context.login(userData);
       props.history.push("/");
     },
     onError(err) {
