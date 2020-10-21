8a735fc825f2f44f8a633a81fbaeaccff539aea0
Fatih Karahan <hseyin97@gmail.com>
2020-10-21 15:42:10 +0300

    fixed active page item

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -3,12 +3,14 @@ import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const MenuBar = () => {
-  const [activeItem, setActiveItem] = useState("");
+  const pathName = window.location.pathname;
+  const path = pathName === "/" ? "home" : pathName.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   return (
-    <Menu pointing secondary>
+    <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
         active={activeItem === "home"}
