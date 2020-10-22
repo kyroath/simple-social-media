6e35226b2e5a736e77493edd5b82c4a4b597f01e
Fatih Karahan <hseyin97@gmail.com>
2020-10-22 21:36:59 +0300

    pointed react app to heroku deployment

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/client/react";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://tranquil-anchorage-09814.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
